77216027dd341019751b202b85354eb5e93d8bdd
Enes Kul <kul@widimedia.com>
2016-04-12 16:06:55 +0200

    test

--- app/Http/Controllers/Frontend/InvoicesController.php
+++ app/Http/Controllers/Frontend/InvoicesController.php
@@ -73,6 +73,7 @@
                     $shipmentBuyer->ebay_user_id = $buyer->ebay_user_id;
                     $shipmentBuyer->invoice      = 0;
                 endif;
+                dd($shipment);
                 $shipmentBuyer->first_name = $shipmentFirstName;
                 $shipmentBuyer->last_name  = $request->get('shipment_last_name');
                 $shipmentBuyer->street1    = $request->get('shipment_street1');
